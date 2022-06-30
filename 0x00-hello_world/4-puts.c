308b95e36ef6268b33e0ee7e97a0eb1b7791e387
ChrisDave <daveokai19@gmail.com>
2022-06-30 16:05:07 -0700

    test6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,10 @@
 int main(void)
 
 {
-
+/**
+ * main print out "programming is like building a multilingual puzzle, and then a new line.
+ *
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
