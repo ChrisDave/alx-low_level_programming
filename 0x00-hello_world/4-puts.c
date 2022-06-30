2ecc5e1e8e757fae4f88132ef4cfb68b08fd1308
ChrisDave <daveokai19@gmail.com>
2022-06-30 16:25:04 -0700

    test final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
+/*
+ * File: 4-puts.c
+ * by David Okai
+ */
+
 #include <stdio.h>
 
-int main(void)
-
-{
 /**
- * main print out "programming is like building a multilingual puzzle, and then a new line.
+ * main - Prints Programming is like building a multilingual
+ * 		puzzle, followed by a new line.
  *
+ * Return: Always 0.
  */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
