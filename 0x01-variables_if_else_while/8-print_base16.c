b4efdeab5d41c684654c727cf3e3f63d98f3e1c1
ChrisDave <daveokai19@gmail.com>
2022-07-05 11:43:06 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; ch <= 57; n++)
+{
+		putchar(n);
+}
+	for (ch = 'a'; ch <= 'f'; ch++)
+{		putchar(ch);
+}
+	putchar(10);
+	return (0);
+
+}
