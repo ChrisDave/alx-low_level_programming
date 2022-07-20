5a0418ef4fb00088f296ccc3f18df881f9e3cc0b
ChrisDave <daveokai19@gmail.com>
2022-07-19 22:02:19 -0700

    hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,7 +12,9 @@ void print_chessboard(char (*a)[8])
 	for (index1 = 0; index1 < 8; index1++)
 	{
 		for (index2 = 0; index2 < 8; index2++)
+		{
 			_putchar(a[index1][index2]);
+		}
 
 		_putchar('\n');
 	}
