1ba8d4d23720bb220dc965069196226cfae86f50
ChrisDave <daveokai19@gmail.com>
2022-07-28 06:00:12 -0700

    coins checker

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("%s\n", "Error")'
+		printf("%s\n", "Error");
 			return (1);
 	}
 
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	}
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while (num >= coins[j]);
+		while (num >= coins[j])
 		{
 			num = num - coins[j];
 			res++;
