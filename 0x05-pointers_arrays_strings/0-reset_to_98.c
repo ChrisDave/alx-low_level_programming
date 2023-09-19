0ded729aa628683bbd2073a82c42dc73eeb35d0c
ChrisDave <daveokai19@gmail.com>
2023-09-19 13:49:56 -0700

    a pointer to an int printing 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - a pointer to an int that updates
+ * a value to 98
+ *
+ * @n: integer output
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
