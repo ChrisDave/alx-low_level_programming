7b6d53c382a9cf6e78fd1dda5ad94169753d9ba1
ChrisDave <daveokai19@gmail.com>
2023-10-24 14:49:01 -0700

    corrected description for constant

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * print_listint - Prints all elements of a linked list
+ * print_listint_t - Prints all elements of a linked list
  * @h: head of the list
  *
  * Return: Number of nodes
  */
 size_t print_listint_t(const listint_t *h)
 {
-	register size_t count = 0;
+	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
 	}
-	return (count);
+	return (nodes);
 }
